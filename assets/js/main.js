1c97acdc9530a7af648579afc961bdf7af93686c
Cristian Nistor <contact@nistorcristian.com>
2017-10-22 21:40:08 +0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Main JS file for Haihara
+ */
+
+jQuery(document).ready(function($) {
+
+    var config = {
+        'share-selected-text': true,
+        'load-more': true,
+        'infinite-scroll': true,
+        'infinite-scroll-step': 3,
+        'disqus-shortname': 'hauntedthemes-demo'
+    };
+
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+});
