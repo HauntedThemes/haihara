c90b1a705211900cd234db9302392ef1fe5e89d6
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2017-10-23 17:59:56 +0300

    Improved loop and home.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,12 +14,34 @@ jQuery(document).ready(function($) {
 
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
         h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    if ($('.featured').length) {
+        var swiperFeatured = new Swiper('.featured .swiper-container', {
+            navigation: {
+                nextEl: '.featured .swiper-button-next',
+                prevEl: '.featured .swiper-button-prev',
+            },
+            spaceBetween: 30,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false
+            },
+        });
+
+        swiperFeatured.on('slideChange', function(event) {
+            $('.featured-navigation li').removeClass('active');
+            $('.featured-navigation li:nth-child('+ (swiperFeatured.activeIndex+1) +')').addClass('active');
+        });
+
+        $('.featured-navigation li:first-child').addClass('active');
+
+        $('.featured-navigation li').each(function(index, el) {
+            $(this).on('click', function(event) {
+                event.preventDefault();
+                swiperFeatured.slideTo(index);
+            });
+        });
+    };
     
-    var swiper = new Swiper('.swiper-container', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
 
 });
