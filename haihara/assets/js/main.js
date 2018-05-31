2937e059d7d22be45350e3151c670c7f878f11e9
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2018-05-31 17:29:20 +0300

    Improved translation.

--- haihara/assets/js/main.js
+++ haihara/assets/js/main.js
@@ -63,7 +63,6 @@ jQuery(document).ready(function($) {
 
     if ($('.recent-slider').length) {
 
-        var nextPage = 3;
         var pagination = 4;
 
         var swiperRecent = new Swiper('.recent-slider .swiper-container', {
@@ -83,130 +82,12 @@ jQuery(document).ready(function($) {
                 },
             }
         });
-        swiperRecent.on('reachEnd', function(event) {
-
-            var parseUrl = '&include=tags&limit=' + pagination + '&page=' + nextPage + '&filter=featured:false';
-            if ($('body').attr('data-author')) {
-                parseUrl = parseUrl + '+author:' + $('body').attr('data-author');
-            }else if($('body').attr('data-tag')){
-                parseUrl = parseUrl + '+tag:' + $('body').attr('data-tag');
-            }
-
-            $.ajax({
-                url: ghost.url.api("posts") + parseUrl,
-                type: 'get'
-            }).done(function(data) {
-                $.each(data.posts, function(i, post) {
-                    $.ajax({
-                        url: ghost.url.api("users") + '&filter=id:' + post.author,
-                        type: 'get'
-                    }).done(function(data) {
-                        $.each(data.users, function(i, users) {
-                            insertPost(post, users, swiperRecent);
-                        });
-                    });
-                });
-                nextPage += 1;
-            }).fail(function(err) {
-                console.log(err);
-            });
-
-        });
 
         if ($('.recent-slider .swiper-slide').length <= pagination) {
             $('.recent-slider .swiper-button-next, .recent-slider .swiper-button-prev').hide();
         };
 
     };
-
-    // Get first number of words from a string
-    function getWords(str) {
-        return str.split(/\s+/).slice(0,40).join(" ");
-    }
-
-    // Append posts on masonry container
-    function insertPost(postData, authorData, swiperRecent) {
-
-        var d = postData.published_at.slice(0, 10).split('-');
-        var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        var monthNumber = d[1];
-        if (monthNumber.slice(0,1) == '0') {
-            monthNumber = monthNumber.slice(1,2) - 1;
-        }else{
-            monthNumber--;
-        };
-
-        var featured = '';
-
-        if (postData.featured) {
-            featured = 'featured';
-        };
-
-        if (d[2].slice(0,1) == '0') {
-            d[2] = d[2].slice(1,2);
-        }
-
-        var datetime = d[0] +'-'+ d[1] +'-'+ d[2];
-        var date = monthNames[monthNumber] + ' ' + d[2] + ', ' + d[0];
-        var excerpt;
-        if (postData.custom_excerpt != null) {
-            excerpt = postData.custom_excerpt;
-        }else{
-            excerpt = getWords($(postData.html).text());
-        };
-
-        var data = {
-            comment_id: postData.comment_id,
-            title: postData.title,
-            date: {
-                "datetime": datetime,
-                "date": date
-            },
-            featured: featured,
-            url: postData.url,
-            excerpt: excerpt,
-            author: {
-                "slug": authorData.slug,
-                "name": authorData.name
-            },
-            tags: function(){
-                if (!$.isEmptyObject(postData.tags)) {
-                    data.tags.tag = postData.tags.slice( 0, 5 );
-                    return true;
-                };
-            },
-            feature_image: function(){
-                if (postData.feature_image != '' && postData.feature_image != null) {
-                    return postData.feature_image;
-                };
-            },
-        }
-
-        var template = [
-            '<article class="post post-card {{#featured}}featured{{/featured}} {{^feature_image}}no-image{{/feature_image}} {{#tags}}{{#tags.tag}}tag-{{slug}} {{/tags.tag}}{{/tags}}" data-id="{{comment_id}}">',
-                '<div class="content">',
-                    '<div class="content-holder">',
-                        '<h3 class="post-title"><a href="{{url}}" title="{{title}}">{{title}}</a></h3>',
-                        '<p>',
-                            '{{excerpt}}',
-                        '</p>',
-                        '<a href="{{url}}" title="{{title}}" class="read-more">Read more</a>',
-                    '</div>',
-                    '{{#feature_image}}',
-                        '<div class="img-holder">',
-                            '<a href="{{url}}" title="{{title}}">',
-                                '<span style="background-image: url({{feature_image}})"></span>',
-                            '</a>',
-                        '</div>',
-                    '{{/feature_image}}',
-                '</div>',
-            '</article>'
-        ].join("\n");
-
-        var post = Mustache.render(template, data);
-        post = $(post);
-        swiperRecent.appendSlide('<div class="swiper-slide">' + post[0].outerHTML + '</div>');
-    }
 
     // Menu trigger
     $('.nav-trigger').on('click', function(event) {
@@ -437,7 +318,7 @@ jQuery(document).ready(function($) {
                 offset_top: 15
             });
             if (currentPage == maxPages) {
-                $('#load-posts').addClass('last').text('No more posts');
+                $('#load-posts').addClass('last').text($('#load-posts').attr('data-end'));
             };
             lastDocumentHeight = $document.height();
             isLoading = false;
@@ -478,7 +359,7 @@ jQuery(document).ready(function($) {
                     offset_top: 15
                 });
                 if (currentPage == maxPages) {
-                    $('#load-posts').addClass('last').text('No more posts');
+                    $('#load-posts').addClass('last').text($('#load-posts').attr('data-end'));
                 };
                 lastDocumentHeight = $document.height();
                 isLoading = false;
@@ -489,14 +370,6 @@ jQuery(document).ready(function($) {
 
         infiniteScroll();
     };
-
-    // Hide widget if no posts
-    $('.widget').each(function(index, el) {
-        var count = $(this).find('li').length;
-        if (count == 0) {
-            $(this).remove();
-        };
-    });
 
     // Validate Subscribe input
     $('.gh-signin').on('submit', function(event) {
